979f4f5e1f64ff3bc6034be2703c441fe051e09c
PeterMagPeter <petermagpeter546@gmail.com>
2024-06-15 19:56:42 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
 /* istanbul ignore file */
 
 import dotenv from "dotenv";
-dotenv.config(); // read ".env"
+dotenv.config() // read ".env"
 import express from 'express';
 import cors from 'cors';
 import http from "http";
 import mongoose from 'mongoose';
 import app from "./app";
-import { logger } from "./logger";
+import { logger } from "./logger"
 import { readFile } from "fs/promises";
 import https from "https";
 import { startWebSocketConnection } from "./websockets";
 
 /** 
  * Init setup to connect to MongoDB
- */ 
+ * */ 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = `mongodb+srv://TestUser:ThisIsPassword@oceancombat.3shvrzt.mongodb.net/?retryWrites=true&w=majority&appName=OceanCombat`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=OceanCombat`;
+
+/**
+ * Test .env values
+ */
+// console.log("Username: " + process.env.MONGO_USER)
+// console.log("PW: " + process.env.MONGO_PW)
+// console.log("Cluster: " + process.env.MONGO_CLUSTER)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,75 +33,76 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   }
+  //tls: true  // Etablishes TLS connection
 });
 
-async function setup() {
-  let mongodURI = process.env.DB_CONNECTION_STRING || "test";
-  
-  if (!mongodURI) {
-    logger.error(`Cannot start, no database configured. Set environment variable DB_CONNECTION_STRING. Use "memory" for MongoMemoryServer, anything else for real MongoDB server`);
-    process.exit(1);
-  }
 
-  if (mongodURI === "memory") {
-    logger.info("Start MongoMemoryServer");
-    const MMS = await import('mongodb-memory-server');
-    const mongo = await MMS.MongoMemoryServer.create();
-    mongodURI = mongo.getUri();
+    async function setup() {
 
-    logger.info(`Connect to mongod at ${mongodURI}`);
-    await mongoose.connect(mongodURI);
-  
-    const shouldSSL = true;
-    if (shouldSSL) {
-      const [privateKey, publicSSLCert] = await Promise.all([
-        readFile('./cert/private.key'),
-        readFile('./cert/public.crt')
-      ]);
+    let mongodURI = process.env.DB_CONNECTION_STRING;
+    if (!mongodURI) {
+        logger.error(`Cannot start, no database configured. Set environment variable DB_CONNECTION_STRING. Use "memory" for MongoMemoryServer, anything else for real MongoDB server"`);
+        process.exit(1);
+    }
+    if (mongodURI === "memory") {
+        logger.info("Start MongoMemoryServer")
+        const MMS = await import('mongodb-memory-server')
+        const mongo = await MMS.MongoMemoryServer.create();
+        mongodURI = mongo.getUri();
 
-      const httpsServer = https.createServer({ key: privateKey, cert: publicSSLCert }, app);
-      const HTTPS_PORT = 3001;
-      httpsServer.listen(HTTPS_PORT, () => {
-        console.log(`Listening for HTTPS at https://localhost:${HTTPS_PORT}`);
-      });
+        logger.info(`Connect to mongod at ${mongodURI}`)
+        await mongoose.connect(mongodURI);
+    
+        const shouldSSL = process.env.USE_SSL === "true";
+        if(shouldSSL) {
+            const [privateKey, publicSSLCert] = await Promise.all([
+                readFile(process.env.SSL_KEY_FILE!),
+                readFile(process.env.SSL_CRT_FILE!)
+            ]);
+    
+            const httpsServer = https.createServer({key: privateKey, cert: publicSSLCert}, app);
+            const HTTPS_PORT = parseInt(process.env.HTTPS_PORT!);
+            httpsServer.listen(HTTPS_PORT, () => {
+                console.log(`Listening for HTTPS at https://localhost:${HTTPS_PORT}`);
+                
+            })
+        } else {
+            const port = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 3000;
+            const httpServer = http.createServer(app);
+            startWebSocketConnection(httpServer);
+            httpServer.listen(port, () => {
+                logger.info(`Listening for HTTP at http://localhost:${port}`);
+            });
+        }
     } else {
-      const port = 3000;
-      const httpServer = http.createServer(app);
-      startWebSocketConnection(httpServer);
-      httpServer.listen(port, () => {
-        logger.info(`Listening for HTTP at http://localhost:${port}`);
-      });
+      try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect().then(()=>{console.log("Connection successfully etablished!");});
+        
+        // Send a ping to confirm a successful connection
+        await client.db("BitBusters").command({ ping: 1 }).then(()=>{console.log("Pinged your deployment. You successfully connected to MongoDB!");});
+
+        mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from DB');
+});
+
+      } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+      }
+        const expressServer = app.listen(process.env.SERVER_PORT || 3001, () => {
+    console.log('Server Started PORT ==> ', process.env.SERVER_PORT || 3001);
+  });
+  startWebSocketConnection(expressServer);
     }
-  } else {
-    try {
-      // Connect the client to the server (optional starting in v4.7)
-      await client.connect().then(() => { console.log("Connection successfully established!"); });
-      
-      // Send a ping to confirm a successful connection
-      await client.db("BitBusters").command({ ping: 1 }).then(() => { console.log("Pinged your deployment. You successfully connected to MongoDB!"); });
-
-      mongoose.connection.on('connected', () => {
-        console.log('Mongoose connected to DB');
-      });
-
-      mongoose.connection.on('error', (err) => {
-        console.error('Mongoose connection error:', err);
-      });
-
-      mongoose.connection.on('disconnected', () => {
-        console.log('Mongoose disconnected from DB');
-      });
-
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
-
-    const expressServer = app.listen(3001, () => {
-      console.log('Server Started PORT ==> 3001');
-    });
-    startWebSocketConnection(expressServer);
-  }
-}
+};
 
 setup().catch(console.dir);
